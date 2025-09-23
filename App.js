abeb91d4176b3915114d8b7f90396e4428d606f5
evelynM07 <167575437+evelynM07@users.noreply.github.com>
2025-09-23 17:24:47 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "./screens/login";
 import RegisterScreen from "./screens/RegisterScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-    return <RegisterScreen />;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
